ec5001d246d4d033ab06394067a4967b0876ed5a
Fahari <karonjekevin67@gmail.com>
2019-01-22 18:12:16 +0300

    import components to app-routing.modules.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GithubSearchComponent } from './github-search/github-search.component';
+import { GithubUsersComponent } from './github-users/github-users.component';
+import { GithubRepoComponent } from './github-repo/github-repo.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [{
+path: '',component:GithubSearchComponent
+},
+{
+   path: 'users',component:GithubUsersComponent
+},
+{
+  path: 'repo',component:GithubRepoComponent
+
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
